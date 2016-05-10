4051061b207421fc32aa9e8fe55ffc685d84502a
ZhaoDong1987 <517425188@qq.com>
2016-05-10 14:07:51 +0800

    Signed-off-by: ZhaoDong1987 <517425188@qq.com>

---
+++ src/testgithub3/testgithub3.java
@@ -0,0 +1,10 @@
+package testgithub3;
+
+public class testgithub3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test-----github3");
+	}
+
+}
