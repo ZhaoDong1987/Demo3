dae56f22f6b5da748d48dbda4d068a0e69a4df69
ZhaoDong1987 <517425188@qq.com>
2016-05-16 11:00:54 +0800

    Signed-off-by: ZhaoDong1987 <517425188@qq.com>

--- src/testgithub3/testgithub3.java
+++ src/testgithub3/testgithub3.java
@@ -1,10 +1,16 @@
 package testgithub3;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class testgithub3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test-----github3");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		System.out.println(screenSize.width);
+		System.out.println(screenSize.height);
 	}
 
 }
